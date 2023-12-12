d46e841e89587284ed6f26fd7b24c71ef8eb55e7
Oldecrinion <zashim@mail.ru>
2023-12-12 17:07:03 +0500

    commit 2

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,13 @@
 ﻿#include <iostream>
 
+using namespace std;
+
 int main()
 {
+    int a, b;
+    cout << "chislo" << endl;
+    cin >> a;
+    cin >> b;
+    cout << endl << "chisla   " << a << " " << b << endl;
     
 }
